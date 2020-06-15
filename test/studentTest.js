0e9812f42bb379d0f2f87e6730e0245ec007c91d
Jacob Gutierrez <gutierr8@southwestern.edu>
2020-06-15 09:16:36 -0500

    Test github action workflow

--- test/studentTest.js
+++ test/studentTest.js
@@ -6,25 +6,32 @@ var assert = chai.assert;
 chai.use(chaiHttp);
 
 describe('students', () => {
-    it('GET / : all students with no students', done => {
-        chai.request(server)
-        .get('/api/students')
-        .end((err, res) => {
-            assert.equal(res.status, 200, "Status should be 200");
-            assert.property(res.body, 'docs', "Response should have property 'docs'");
-            assert.typeOf(res.body.docs, 'array', "'docs' should be an array");
-            assert.lengthOf(res.body.docs, 0, "'docs' should be of length 0");
-            done();
-        });
-    });
-
-    it('GET /:id : get one student error', done => {
-        chai.request(server)
-            .get('/api/students/0')
+    describe('GET / and /:id ', () => {
+        it('GET / : all students with no students', done => {
+            chai.request(server)
+            .get('/api/students')
             .end((err, res) => {
-                assert.equal(res.status, 404, "Status should be 404");
-                assert.property(res.body, 'message', "Body should have 'message' property");
+                assert.equal(res.status, 200, "Status should be 200");
+                assert.property(res.body, 'docs', "Response should have property 'docs'");
+                assert.typeOf(res.body.docs, 'array', "'docs' should be an array");
+                assert.lengthOf(res.body.docs, 0, "'docs' should be of length 0");
                 done();
             });
+        });
+    
+        it('GET /:id : get one student error', done => {
+            chai.request(server)
+                .get('/api/students/0')
+                .end((err, res) => {
+                    assert.equal(res.status, 404, "Status should be 404");
+                    assert.property(res.body, 'message', "Body should have 'message' property");
+                    done();
+                });
+        });
+    });
+    describe('Should fail', () => {
+        it('stop github action workflow', done => {
+            assert.fail();
+        });
     });
 });
