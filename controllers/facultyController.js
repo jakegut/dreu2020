e78d4615d230bc52a7ef3c73e6b2dbe982f85fde
Jacob Gutierrez <gutierr8@southwestern.edu>
2020-06-15 15:28:48 -0500

    Fix error in faculty controller

--- controllers/facultyController.js
+++ controllers/facultyController.js
@@ -1,21 +1,21 @@
 const Faculty = require('../models/faculty');
 
 exports.getAllFaculty = (req, res) => {
-    Faculty.find({}, (err, docs){
+    Faculty.find({}, (err, docs) => {
         if(err) res.status(400).json(err);
         res.json({docs});
     });
 }
 
 exports.getOneFaculty = (req, res) => {
-    Faculty.findById(req.params.id, (err, docs){
+    Faculty.findById(req.params.id, (err, docs) => {
         if(err) return res.status(404).json({message: "Faculty not found"})
     });
 }
 
 exports.postOneFaculty = (req, res) => {
     let {firstName, lastName, email, department} = req.body;
-    Faculty.create({firstName, lastName, email, department}, (err, docs){
+    Faculty.create({firstName, lastName, email, department}, (err, docs) => {
         if(err) return res.status(400).json(err);
         res.status(201).json({docs, message: "Faculty created"})
     });
